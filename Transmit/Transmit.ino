3598f557145335febcffeb8663b22849488ec334
Jerry Bai <jerrybai1@hotmail.com>
2016-11-23 21:30:26 -0500

    save point

--- Transmit/Transmit.ino
+++ Transmit/Transmit.ino
@@ -21,7 +21,7 @@ char rx_byte = 0;
 
   if (Serial.available() > 0) {    // is a character available?
     rx_byte = Serial.read();       // get the character
-  
+
   if (rx_byte == 'a'){
     tone(11, 6000);
   }
@@ -33,38 +33,147 @@ char rx_byte = 0;
 
 #include <SoftwareSerial.h>
 
-SoftwareSerial serial_MasterSlave(10, 11); //RX, TX
-SoftwareSerial serial_URL (5, 6); //Rx, Tx (5 not used)
+//Pin stuff
+int pin_masterTX = 11;
+int pin_slaveRX = 10;
+int pin_URL = 6;
+int pin_null = 5;
+int pin_waveAB = 3;
+SoftwareSerial serial_MasterSlave(pin_slaveRX, pin_masterTX); //RX, TX
+SoftwareSerial serial_URL (pin_null, pin_URL); //Rx, Tx (RX not used)
 
-enum MODE {MASTER, SLAVE};
-MODE mode = SLAVE; //default
+//defs
+enum MODE {MASTER, SLAVE} mode;
+int channel1 = 5475; //Hz
+int channel2 = 600; //Hz
+
+//function declares
+void sendWaveA(int freq);
+void sendWaveB(int freq);
 
 void setup() {
-  Serial.begin(4800);
-  mySerial.begin(9600); //for URL transmission
+  Serial.begin(4800); //local
+  serial_URL.begin(4800); //for URL transmission
+  serial_MasterSlave.begin(11920); //to ping slave (needs to be fast)
+
+  mode = SLAVE; //default
+  Serial.println("Mode: Slave");
 }
 
 void loop() {
-}
-  //settings change
-  if (Serial.available()){
-    string input = Serial.readString();
-    input.toLowerCase();
-    
-    if (input.equals("master")
-      mode = MASTER;
+   //settings change
+    if (Serial.available()){
+        String input = Serial.readString();
+        input.toLowerCase();
 
-    if (input.equals("slave")
-      mode = SLAVE;
-  }
-  
-  //main
-  if (mode=MASTER)
-    
-  {
-  } else {
-    //slave
-    if
-  }
+        if (input.equals("master")) {
+            Serial.println("Mode: Master");
+            mode = MASTER;
+        }
+
+        if (input.equals("slave")) {
+            Serial.println("Mode: Slave");
+            mode = SLAVE;
+        }   
+
+        if (mode==MASTER){
+            if (input.equals("url")){
+                Serial.print("Input Url: ");
+                while (!Serial.available()) {
+                    //wait
+                }
+                String url = Serial.readString();
+                Serial.print("Sending Url: ");
+                Serial.println(url);
+
+                char buf[50];
+                url.toCharArray(buf, 50); 
+                serial_URL.write(buf);
+            } else if  (input.equals("ab")){
+                //ping slave to run B
+                serial_MasterSlave.write('b');
+                //run A on channel 1
+                sendWaveA(channel1);
+            } else if  (input.equals("ba")){
+                //ping slave to run A
+                serial_MasterSlave.write('a');
+                //run B on channel 1
+                sendWaveB(channel1);
+
+            } else if (!input.equals("master")){
+                Serial.println("Not valid. Please enter MASTER, SLAVE, URL, AB, or BA");
+            }
+        }
+    }
+
+    if (mode==SLAVE){
+        //slave
+        if (serial_MasterSlave.available()){
+      
+            //read letter
+            char letter = serial_MasterSlave.read();
+            Serial.print("Received letter");
+            Serial.println(letter);
+      
+            //send on channel2
+            if (letter == 'a') {
+                sendWaveA(channel2);
+            }
+            if (letter == 'b') {
+                sendWaveB(channel2);
+            }
+        }
+    }
 }
 
+void sendWaveA(int freq) {
+    Serial.print("Sending wave A at ");
+    Serial.print(freq);
+    Serial.println(" Hz");
+
+    tone(pin_waveAB, freq); //todo make it into pattern A
+}
+
+void sendWaveB(int freq){
+    Serial.print("Sending wave B at ");
+    Serial.print(freq);
+    Serial.println(" Hz");
+
+    tone(pin_waveAB, freq); //todo make it into pattern B
+}
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
